c3861e442970cfac145c8a38267ac0d2adc87da4
Macbook <macbook@Macbooks-MacBook-Air.local>
2020-07-20 17:07:44 -0400

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardImg /*CardImgOverlay*/, CardText, CardBody, Button, CardTitle, Breadcrumb, BreadcrumbItem, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label, Row, Col} from 'reactstrap';
+import { Card, CardImg /*CardImgOverlay*/, CardText, CardBody, Button, Breadcrumb, BreadcrumbItem, Modal, ModalHeader, ModalBody, Label, Row, Col} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors, } from 'react-redux-form';
 
@@ -19,7 +19,7 @@ function RenderCampsite({ campsite }) {
     );
 }
 
-function RenderComments({ comments }) {
+function RenderComments({comments, addComment, campsiteId}) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -35,7 +35,7 @@ function RenderComments({ comments }) {
                         </div>
                     );
                 })}
-                <CommentForm />
+                <CommentForm campsiteId={campsiteId} addComment={addComment} />
             </div >
         );
     }
@@ -56,9 +56,9 @@ class CommentForm extends React.Component {
         });
     }
     handleSubmit(values) {
-        console.log('Current state is: ' + JSON.stringify(values));
-        alert('Current state is: ' + JSON.stringify(values));
-    }
+        this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+    }     
     render() {
         return (
             <div className="col">
@@ -109,9 +109,9 @@ class CommentForm extends React.Component {
                                 </Col>
                             </Row>
                             <Row className="form-group">
-                                <Label htmlFor="comment" md={12}>Comment</Label>
+                                <Label htmlFor="text" md={12}>Comment</Label>
                                 <Col md={12}>
-                                    <Control.textarea model=".comment" id="comment" name="comment"
+                                    <Control.textarea model=".text" id="text" name="text"
                                         rows="12"
                                         className="form-control"
                                     />
@@ -149,7 +149,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments 
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
 
                 </div>
             </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,6 +8,8 @@ import Home from './HomeComponent';
 import About from './AboutComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
+import { addComment } from '../redux/ActionCreators';
+
 
 const mapStateToProps = state => {
     return {
@@ -15,8 +17,13 @@ const mapStateToProps = state => {
         comments: state.comments,
         partners: state.partners,
         promotions: state.promotions
-    };
+    }
 };
+
+const mapDispatchToProps = {
+    addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text))
+};
+
 
 class Main extends Component {
 
@@ -36,9 +43,10 @@ class Main extends Component {
         const CampsiteWithId = ({match}) => {
             return (
                 <CampsiteInfo 
-                    campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
-                    comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
-                />
+                campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]} 
+                comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                addComment={this.props.addComment}
+            />
             );
         };    
         return (
@@ -58,4 +66,4 @@ class Main extends Component {
     }
 }
 
-export default withRouter(connect(mapStateToProps) (Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
