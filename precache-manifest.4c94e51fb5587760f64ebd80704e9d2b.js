8c8dce94aceb22aa3c8d65770164bffc9604f4d5
Macbook <macbook@Macbooks-MacBook-Air.local>
2020-07-21 22:08:21 -0400

    Updates

---
+++ precache-manifest.4c94e51fb5587760f64ebd80704e9d2b.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e676e4e079129f4d2394a691718c802",
+    "url": "/ReactCamp/index.html"
+  },
+  {
+    "revision": "954072106c68dbdb4f91",
+    "url": "/ReactCamp/static/css/2.68eade42.chunk.css"
+  },
+  {
+    "revision": "f3bb563d4e370c58e96a",
+    "url": "/ReactCamp/static/css/main.90f1204c.chunk.css"
+  },
+  {
+    "revision": "954072106c68dbdb4f91",
+    "url": "/ReactCamp/static/js/2.cef56abf.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/ReactCamp/static/js/2.cef56abf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3bb563d4e370c58e96a",
+    "url": "/ReactCamp/static/js/main.43a4a11e.chunk.js"
+  },
+  {
+    "revision": "f6367733913dcaa48a8f",
+    "url": "/ReactCamp/static/js/runtime-main.1a39fb49.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ReactCamp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ReactCamp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ReactCamp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ReactCamp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ReactCamp/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "02bba601716f9a0ff1e241a64cfe9b29",
+    "url": "/ReactCamp/static/media/lobster-latin-400.02bba601.woff"
+  },
+  {
+    "revision": "fe012fb7b22e5776c4a2e91889068974",
+    "url": "/ReactCamp/static/media/lobster-latin-400.fe012fb7.woff2"
+  },
+  {
+    "revision": "24f7b0944e9e03a905f9d7701573b2cd",
+    "url": "/ReactCamp/static/media/open-sans-latin-300.24f7b094.woff2"
+  },
+  {
+    "revision": "a7622f60c56ddd5301549a786b54e6e6",
+    "url": "/ReactCamp/static/media/open-sans-latin-300.a7622f60.woff"
+  },
+  {
+    "revision": "6725c3fb1f3abd7ebdcb2d244758f706",
+    "url": "/ReactCamp/static/media/open-sans-latin-300italic.6725c3fb.woff2"
+  },
+  {
+    "revision": "793b1237017aeacd646fb80911425566",
+    "url": "/ReactCamp/static/media/open-sans-latin-300italic.793b1237.woff"
+  },
+  {
+    "revision": "33543c5cc5d88f5695dd08c87d280dfd",
+    "url": "/ReactCamp/static/media/open-sans-latin-400.33543c5c.woff2"
+  },
+  {
+    "revision": "de0869e324680c99efa1250515b4b41c",
+    "url": "/ReactCamp/static/media/open-sans-latin-400.de0869e3.woff"
+  },
+  {
+    "revision": "06b4bfda4e139eaf3ab9872a6d66f42f",
+    "url": "/ReactCamp/static/media/open-sans-latin-400italic.06b4bfda.woff"
+  },
+  {
+    "revision": "b61a9055d92ac56c15cacf4271d4f072",
+    "url": "/ReactCamp/static/media/open-sans-latin-400italic.b61a9055.woff2"
+  },
+  {
+    "revision": "449d681cd6006390e1bee3c3a660430b",
+    "url": "/ReactCamp/static/media/open-sans-latin-600.449d681c.woff"
+  },
+  {
+    "revision": "819af3d3abdc9f135d49b80a91e2ff4c",
+    "url": "/ReactCamp/static/media/open-sans-latin-600.819af3d3.woff2"
+  },
+  {
+    "revision": "56e5756b696615d6164a625e1bcb1a9e",
+    "url": "/ReactCamp/static/media/open-sans-latin-600italic.56e5756b.woff"
+  },
+  {
+    "revision": "ebdfa24cb411ba3016abd1c3ea5746e5",
+    "url": "/ReactCamp/static/media/open-sans-latin-600italic.ebdfa24c.woff2"
+  },
+  {
+    "revision": "0edb76284a7a0f8db4665b560ee2b48f",
+    "url": "/ReactCamp/static/media/open-sans-latin-700.0edb7628.woff2"
+  },
+  {
+    "revision": "1f85e92d8ff443980bc0f83ad7b23b60",
+    "url": "/ReactCamp/static/media/open-sans-latin-700.1f85e92d.woff"
+  },
+  {
+    "revision": "a49393f2171a2f4d6927ce6c14f73dbc",
+    "url": "/ReactCamp/static/media/open-sans-latin-700italic.a49393f2.woff2"
+  },
+  {
+    "revision": "bf72679ca22e53320beaea090e8bb07d",
+    "url": "/ReactCamp/static/media/open-sans-latin-700italic.bf72679c.woff"
+  },
+  {
+    "revision": "05ebdbe10796850f045fcd484f35788d",
+    "url": "/ReactCamp/static/media/open-sans-latin-800.05ebdbe1.woff"
+  },
+  {
+    "revision": "ab3e500e7375695d702cff19513e4470",
+    "url": "/ReactCamp/static/media/open-sans-latin-800.ab3e500e.woff2"
+  },
+  {
+    "revision": "92da6f116d973bd334cf9b3afdb29c4f",
+    "url": "/ReactCamp/static/media/open-sans-latin-800italic.92da6f11.woff"
+  },
+  {
+    "revision": "c62c3eea77cf3fca51494e8c924adf4d",
+    "url": "/ReactCamp/static/media/open-sans-latin-800italic.c62c3eea.woff2"
+  }
+]);
